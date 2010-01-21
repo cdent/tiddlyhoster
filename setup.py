40a7c7d763b973986a4c4c14120144b7acb285b8
Chris Dent <cdent@oxide.local>
2010-01-21 20:25:01 +0000

    add some text to the recipe ordering window

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ DESCRIPTION = 'A hoster for TiddlyWikis'
 import os
 from setuptools import setup, find_packages
 
-VERSION = '0.9.2'
+VERSION = '0.9.4'
 
 
 # You should review the below so that it seems correct. install_requires
